c6197c5c06778bb7dec10c199363b343435a9372
Connor Heckley <58855366+connorheckley@users.noreply.github.com>
2021-02-13 21:33:23 +0000

    Add files via upload

---
+++ quickref/quickref.py
@@ -0,0 +1,55 @@
+# set1 = {'cat', 'dog', 'mouse', 'lion'}  
+# set2 = {1,2,3,4,5,6,7} 
+# print(set1)
+
+# my_list = [1,2,3,4,5]
+
+# print(my_list)
+
+# empty_dict = {'column 1': 'row1', 'row2','row3','row4',
+#               'column 2': 'row1', 'row2','row3','row4',
+#               'column 3': 'row1', 'row2','row3','row4',
+#               'column 4': 'row1', 'row2','row3','row4',
+#               'column 5': 'row1', 'row2','row3','row4',
+#               'column 6': 'row1', 'row2','row3','row4', }
+
+# info = {'Radek':['0123456789', '31 House'], 
+#         'Connor':['07507339417','34 House'],
+#         'Tina':'0768996454'}
+# print(info)
+
+# info['Deacon'] = ['987987897', 'address']
+# info['Thomas'] = ['777888999', 'address']
+# info.update({'Radek': ['0123456789','4 House']})
+# print(info)
+
+# name = [1, 'con', 2,3,4,5,6]
+
+# print(8 in name)
+
+# print('-----'.join(['Radek', 'Connor', 'Tina']))
+
+# name = """ Radek
+# Warowny
+# Connor
+# Tina"""
+
+# find_last = name.endswith('a')
+# print(find_last)
+# find_first = name.startswith('R')
+# print(find_first)
+
+my_list = [4,3,8,5,8,9,4]
+
+my_list.sort()
+
+print(my_list)
+
+print(my_list.count(4))
+
+# x = 0
+# for i in range(10):
+#     x = i * 10
+#     if i == 5:
+#     	break
+#     print(x)
